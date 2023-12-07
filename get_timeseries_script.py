6e30e28bd5da1441c262732d72c906eb02674880
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-08 07:21:32 +1300

    Finished script

--- get_timeseries_script.py
+++ get_timeseries_script.py
@@ -7,10 +7,14 @@ import yfinance as yf
 # ~~~~~~~~~~~~~~~~~~~
 #       INPUTS
 # ~~~~~~~~~~~~~~~~~~~
-TICKER = "AAPL"
-DATES = ()
+# This will collect the past 3 years of data from yfinance
+# and will collect all data for the [TICKER] from the SEC
+TICKER = "ZYXI"
+
+# Whether to download a filtered SEC document (filtered with ["label", "end", "form", "fp"] as the unique key)
+DOWNLOAD_FILTERED = True
 # ~~~~~~~~~~~~~~~~~~~
-#       INPUTS
+#       
 # ~~~~~~~~~~~~~~~~~~~
 
 
@@ -23,16 +27,19 @@ cik = mapper.ticker_to_cik[TICKER]
 headers = {"User-Agent": "stirlingjamessmith@gmail.com"}
 api_url = 'https://data.sec.gov/api/xbrl/companyfacts/CIK{}.json'.format(cik)
 
-response = requests.get("https://data.sec.gov/api/xbrl/companyfacts/CIK0000789019.json", headers=headers).text
+response = requests.get(api_url, headers=headers).text
 contents = json.loads(response)
 
 company_name = contents["entityName"]
 
-labeled_dicts = contents["facts"]["us-gaap"]
-labels = list(labeled_dicts.keys())
+print("Downloading Data for",company_name)
 
-labeled_dicts_small = contents["facts"]["dei"]
-labels_small = list(labeled_dicts_small.keys())
+# SEC Data is split into 2 sections us-gaap and dei
+labeled_dicts_gaap = contents["facts"]["us-gaap"]
+labels_gaap = list(labeled_dicts_gaap.keys())
+
+labeled_dicts_dei = contents["facts"]["dei"]
+labels_dei = list(labeled_dicts_dei.keys())
 
 def labeled_to_df(labeled_dicts, label):
 
@@ -41,36 +48,34 @@ def labeled_to_df(labeled_dicts, label):
     def filter_dict(dicts, k): return [dict[k] for dict in dicts]
 
     for u in units:
-        share_updates = labeled_dicts[label]["units"][u]
+        sample_dict = labeled_dicts[label]["units"][u]
         keys = ["val", "filed", "form", "end", "fp"]
 
-        dicts = [{k: filter_dict(share_updates, k)} for k in keys]
+        dicts = [{k: filter_dict(sample_dict, k)} for k in keys]
         unit_df=pd.DataFrame({**dicts[0], **dicts[1], **dicts[2], **dicts[3], **dicts[4]})
 
-        #println(share_updates)
-        # unit_df = vcat(DataFrame.(share_updates)...)
         unit_df["label"]=label
         unit_df["unit"]=u
         dfs.append(unit_df)
     
     return pd.DataFrame(pd.concat(dfs))
 
-dfs = pd.concat([labeled_to_df(labeled_dicts, l) for l in labels])
-dfs_small = pd.concat([labeled_to_df(labeled_dicts_small, l) for l in labels_small])
+df_gaap = pd.concat([labeled_to_df(labeled_dicts_gaap, l) for l in labels_gaap])
+dfs_dei = pd.concat([labeled_to_df(labeled_dicts_dei, l) for l in labels_dei])
 
-big_df = pd.concat([dfs, dfs_small])
+# joining both us-gaap and dei dataframes
+big_df = pd.concat([df_gaap, dfs_dei])
 
 filtered_big_df = big_df.groupby(["label", "end", "form", "fp"]).head(1)
 
 
-#### YFinance
 
 with open("{}_yfinance.csv".format(TICKER), "w") as f:
     f.write(tkr.history(period="3y").to_csv())
 
+if DOWNLOAD_FILTERED:
+    with open("{}_SEC_filtered.csv".format(TICKER), 'w') as f:
+        f.write(filtered_big_df.to_csv(index=False))
 
 with open("{}_SEC.csv".format(TICKER), 'w') as f:
-    f.write(filtered_big_df.to_csv(index=False))
-
-with open("{}_SEC_big.csv".format(TICKER), 'w') as f:
     f.write(big_df.to_csv(index=False))
